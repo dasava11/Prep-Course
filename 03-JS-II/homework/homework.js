bc998d3db5d9ede2f2f70002f1748c53f5c7616f
dasava11 <dasava11@outlook.com>
2022-11-07 21:55:28 -0500

    adelantando tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -111,6 +111,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(Math.floor(numero) === numero) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -118,6 +123,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 3 === 0 && numero % 5 === 0) {
+    return "fizzbuzz"; 
+  } else if(numero % 5 === 0) {
+    return "buzz"
+  } else if(numero % 3 === 0) {
+    return "fizz"
+  } else {
+    return numero
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
